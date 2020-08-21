b5346c84c087364522bfefb4d42a2d268810fa7f
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-08-21 13:35:11 -0400

    Updates

---
+++ precache-manifest.b0ab08bdb020ab1f913714424d8f16b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195844332415bd5f0a809e171f41ef1a",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "18b41e9e865c58692356",
+    "url": "/battleship/static/css/main.51b54349.chunk.css"
+  },
+  {
+    "revision": "534b14fe84914aebcce3",
+    "url": "/battleship/static/js/2.894f067b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.894f067b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b41e9e865c58692356",
+    "url": "/battleship/static/js/main.2310f056.chunk.js"
+  },
+  {
+    "revision": "fa2bb6d32141478767f3",
+    "url": "/battleship/static/js/runtime-main.ceb60dc6.js"
+  }
+]);
